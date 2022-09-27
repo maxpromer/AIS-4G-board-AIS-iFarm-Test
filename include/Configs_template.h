5a656917e4acc19cb0b8dcf04eb2ba05cbce615b
Max <7087907+maxpromer@users.noreply.github.com>
2022-09-28 02:37:58 +0700

    Add configs template file

---
+++ include/Configs_template.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// #define USE_WIFI
+#define USE_4G
+
+#ifdef USE_WIFI
+#define CONFIG_WIFI_SSID "xxxxxxxxxxxxxxx"
+#define CONFIG_WIFI_PASS "xxxxxxxxxxxxxxx"
+#endif
+
+// Magellan
+#define CONFIG_THING_IDENTIFY "xxxxxxxxxxxxxxx"
+#define CONFIG_THING_SECRET   "xxxxxxxxxxxxxxx"
+#define CONFIG_THING_TOKEN    "xxxxxxxxxxxxxxx"
+#define CONFIG_THING_NAME     "xxxxxxxxxxxxxxx"
+
+#define BUILTIN_LED (15)
